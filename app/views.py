24bd1fb447d1c4522f032205c2a0328ee5563de9
smartboy6345 <duddujeevankumar@gmail.com>
2023-11-20 23:00:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kanth(request):
+    return render(request,'kanth.html')
+def srujana(request):
+    return render(request,'srujana.html')
+def person(request):
+    return render(request,'3rdperson.html')
